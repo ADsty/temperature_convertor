34aa6b962de1f435eb0f437ec4eef48f0cba09bd
Petrov Vitaliy <vitalikpetrovdmitrievic@gmail.com>
2021-12-05 21:36:05 +0300

    Adding fahrenheit to kelvin convertor

--- temperature_convertor.py
+++ temperature_convertor.py
@@ -13,6 +13,16 @@ def fahrenheit_to_celsius(value):
     return celsius
 
 
+def fahrenheit_to_kelvin(value):
+    kelvin = (value - 32) * 5 / 9 + 273.15
+    return kelvin
+
+
 def kelvin_to_celsius(value):
     celsius = value - 273.15
     return celsius
+
+
+def kelvin_to_fahrenheit(value):
+    fahrenheit = (value - 273.15) * 9 / 5 + 32
+    return fahrenheit
