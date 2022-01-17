21f1ec750e2c0ea2b92769620ef11f6d54b073e2
Petrov Vitaliy <vitalikpetrovdmitrievic@gmail.com>
2022-01-17 17:13:25 +0300

    Add tests for temperature convertor and refactor main file for testing

--- temperature_convertor.py
+++ temperature_convertor.py
@@ -28,56 +28,59 @@ def kelvin_to_fahrenheit(value):
     return fahrenheit
 
 
-print("Temperature convertor v1.0")
-print("To convert temperature type: [value] [initial temperature unit] [needed temperature unit]")
-print("To stop the program type 'quit' ")
-print("Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
+if __name__ == "__main__":
+    print("Temperature convertor v1.0")
+    print("To convert temperature type: [value] [initial temperature unit] [needed temperature unit]")
+    print("To stop the program type 'quit' ")
+    print("Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
+    while True:
+        input_string = input("Type what do you want: ").strip()
+        parsed_string = input_string.split(" ")
 
+        if input_string == "quit":
+            print("Program was closed")
+            break
 
-while True:
-    input_string = input("Type what do you want: ").strip()
-    parsed_string = input_string.split(" ")
-
-    if input_string == "quit":
-        print("Program was closed")
-        break
-
-    if len(input_string) == 0 or len(parsed_string) != 3:
-        print("Your input is not valid, required format : [value] [initial temperature unit] [needed temperature unit]")
-        print("Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
-        continue
-
-    value = int(parsed_string[0])
-    initial_unit = parsed_string[1]
-    needed_unit = parsed_string[2]
-
-    if initial_unit == "C":
-        if needed_unit == "F":
-            print("Your result is", celsius_to_fahrenheit(value), "F")
-        elif needed_unit == "K":
-            print("Your result is", celsius_to_kelvin(value), "K")
-        else:
-            print("Your needed temperature unit is not valid")
+        if len(input_string) == 0 or len(parsed_string) != 3:
+            print(
+                "Your input is not valid, required format : [value] [initial temperature unit] [needed temperature unit]")
             print("Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
+            continue
 
-    elif initial_unit == "F":
-        if needed_unit == "C":
-            print("Your result is", fahrenheit_to_celsius(value), "C")
-        elif needed_unit == "K":
-            print("Your result is", fahrenheit_to_kelvin(value), "K")
+        value = int(parsed_string[0])
+        initial_unit = parsed_string[1]
+        needed_unit = parsed_string[2]
+
+        if initial_unit == "C":
+            if needed_unit == "F":
+                print("Your result is", celsius_to_fahrenheit(value), "F")
+            elif needed_unit == "K":
+                print("Your result is", celsius_to_kelvin(value), "K")
+            else:
+                print("Your needed temperature unit is not valid")
+                print(
+                    "Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
+
+        elif initial_unit == "F":
+            if needed_unit == "C":
+                print("Your result is", fahrenheit_to_celsius(value), "C")
+            elif needed_unit == "K":
+                print("Your result is", fahrenheit_to_kelvin(value), "K")
+            else:
+                print("Your needed temperature unit is not valid")
+                print(
+                    "Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
+
+        elif initial_unit == "K":
+            if needed_unit == "C":
+                print("Your result is", kelvin_to_celsius(value), "C")
+            elif needed_unit == "F":
+                print("Your result is", kelvin_to_fahrenheit(value), "F")
+            else:
+                print("Your needed temperature unit is not valid")
+                print(
+                    "Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
+
         else:
-            print("Your needed temperature unit is not valid")
+            print("Your initial temperature unit is not valid")
             print("Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
-
-    elif initial_unit == "K":
-        if needed_unit == "C":
-            print("Your result is", kelvin_to_celsius(value), "C")
-        elif needed_unit == "F":
-            print("Your result is", kelvin_to_fahrenheit(value), "F")
-        else:
-            print("Your needed temperature unit is not valid")
-            print("Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
-
-    else:
-        print("Your initial temperature unit is not valid")
-        print("Maintained temperature units are: Kelvin (type as K), Celsius (type as C), Fahrenheit (type as F)")
